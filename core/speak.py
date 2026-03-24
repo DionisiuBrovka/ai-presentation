20d6db84658b5032063d65c81dddd8f8b1c5ded5
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2026-03-24 11:29:05 +0300

    закончил с промежуточной версией бэкенда

--- core/speak.py
+++ core/speak.py
@@ -1,17 +1,40 @@
-from silero_tts.silero_tts import SileroTTS
-from utils.utils import form_output_filename
+from loguru import logger
+# Импортируем класс SileroTTS для синтеза речи из текста
+from silero_tts.silero_tts import SileroTTS 
+# Импортируем утилиту для формирования пути к выходному файлу
+from utils.utils import form_output_data
 
-def speak(anser:str) -> str:
+# Создаем отдельный логгер для данного модуля, указывая имя (например, для фильтрации логов)
+speak_module_logger = logger.bind(name="listen_module")
+speak_module_logger.info("INIT")  # Логируем факт инициализации модуля
 
+def speak(answer: str) -> str:
+    """
+    Преобразует переданный текст в аудиофайл (wav) с помощью модели SileroTTS.
+    
+    :param answer: Текстовый ответ, который необходимо преобразовать в речь.
+    :return: Путь к сгенерированному аудиофайлу (формат wav).
+    """
+    
+    # Инициализация синтезатора речи
     tts = SileroTTS(
-        model_id='v5_ru', 
-        language='ru', 
-        sample_rate=24000, 
-        device='cpu', 
-        speaker="eugene"
+        model_id='v5_ru',      # Версия и язык модели (русский)
+        language='ru',
+        sample_rate=24000,     # Частота дискретизации аудио
+        device='cpu',          # Устройство для вычислений (CPU)
+        speaker="eugene"       # Используемый голос для синтеза
     )
 
-    output_filename = form_output_filename("wav")
-    tts.tts(anser, output_filename)
+    speak_module_logger.info("init tts")
 
-    return output_filename
+    # Формируем уникальный путь к выходному аудиофайлу с расширением 'wav'
+    output_filepath = form_output_data()
+
+    speak_module_logger.info("start gen speach")
+    # Синтезируем речь и сохраняем её в указанный файл
+    tts.tts(answer, output_filepath)
+
+    speak_module_logger.info("finish gen speach")
+
+    # Возвращаем путь к готовому аудиофайлу
+    return output_filepath
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,14 @@
 import random
 from datetime import datetime
 
+from werkzeug.datastructures import FileStorage
+
 def get_random_test_audio() -> str:
+    """
+    Возвращает путь к случайному тестовому аудиофайлу из заранее определённого списка.
+    
+    :return: Путь к одному из тестовых аудиофайлов.
+    """
     random_test_audio_list = [
         'test-audio/test-audio-1.ogg',
         'test-audio/test-audio-2.ogg',
@@ -10,12 +17,29 @@ def get_random_test_audio() -> str:
 
     return random.choice(random_test_audio_list)
 
-
-# TODO доделать удаление имени и сохранение формата
-def form_input_filename(filename:str) -> str:
+def form_input_data(file: FileStorage):
+    """
+    Формирует путь и имя для загружаемого входного файла на основе текущей даты и времени.
+    
+    :param file: Объект FileStorage, представляющий загруженный пользователем файл.
+    :return: Кортеж (полный путь к файлу, имя файла, формат файла).
+    """
     now = datetime.now()
-    return f"input/input-{now.hour}_{now.minute}_{now.day}_{now.month}_{now.year}-{filename}"
+    # Извлекаем расширение файла (формат), если оно имеется
+    input_fileformat = file.filename.split('.')[-1] if '.' in file.filename else ''
+    # Генерируем уникальное имя файла с использованием текущего времени и даты
+    input_filename = f"input-{now.hour}_{now.minute}_{now.day}_{now.month}_{now.year}_{now.microsecond}.{input_fileformat}"
+    # Полный путь для сохранения файла
+    input_filepath = f"input/{input_filename}"
 
-def form_output_filename(format:str) -> str:
+    return input_filepath, input_filename, input_fileformat
+
+def form_output_data() -> str:
+    """
+    Формирует путь и имя для выходного (обработанного) аудиофайла, используя текущие дату и время.
+    
+    :return: Полный путь к выходному аудиофайлу формата .wav.
+    """
     now = datetime.now()
-    return f"output/output-{now.hour}_{now.minute}_{now.day}_{now.month}_{now.year}.{format}"
+    # Имя файла с уникальным идентификатором времени
+    return f"output/output-{now.hour}_{now.minute}_{now.day}_{now.month}_{now.year}.wav"
